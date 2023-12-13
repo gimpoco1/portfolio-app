1d8cd7a97288dda059fd9efa2615f29274635ba7
Giacomo Impoco <impoco126@gmail.com>
2023-12-13 13:29:02 +0100

    Adjusted some styling and updated links (projects images missing)

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// SELECTING FIRST LETTER function
+window.onload = function () {
+    var elements = document.querySelectorAll(".each-word");
+
+    for (var i = 0; i < elements.length; i++) {
+        var htmlContent = elements[i].innerHTML;
+        var modifiedContent = htmlContent.replace(
+            /(?:<[^>]*>)|(\b[a-z])([a-z]+)?\b/gim,
+            function (match, p1, p2) {
+                if (p1) {
+                    return "<span class='first-letter'>" + p1 + "</span>" + (p2 || "");
+                } else {
+                    return match; 
+                }
+            }
+        );
+        elements[i].innerHTML = modifiedContent;
+    }
+};
