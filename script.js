286095a1612523ae7a90ab7ff5b757e5ce92f539
Giacomo Impoco <impoco126@gmail.com>
2023-12-13 18:13:41 +0100

    -m

--- script.js
+++ script.js
@@ -26,8 +26,24 @@ window.onload = function () {
         elements[i].innerHTML = modifiedContent;
     }
 };
+// function addDarkmodeWidget() {
+//     new Darkmode().showWidget();
+//   }
+//   window.addEventListener('load', addDarkmodeWidget);
 
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark');
-    console.log('clicked');
-});
+//   const options = {
+//     bottom: '64px', // default: '32px'
+//     right: 'unset', // default: '32px'
+//     left: '32px', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: '#fff',  // default: '#fff'
+//     buttonColorDark: '#100f2c',  // default: '#100f2c'
+//     buttonColorLight: '#fff', // default: '#fff'
+//     saveInCookies: false, // default: true,
+//     label: '🌓', // default: ''
+//     autoMatchOsTheme: true // default: true
+//   }
+  
+//   const darkmode = new Darkmode(options);
+//   darkmode.showWidget();
