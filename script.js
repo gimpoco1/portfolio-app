e98543a1289ef9ef4b77d0d143aab488b7665563
Giacomo Impoco <impoco126@gmail.com>
2023-12-14 18:39:40 +0100

    Refactor HTML and JavaScript code, update CSS styles

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ function toggleMenu() {
 }
 
 // SELECTING FIRST LETTER function
-window.onload = function () {
-    var elements = document.querySelectorAll(".each-word");
+// window.onload = function () {
+//     var elements = document.querySelectorAll(".each-word");
 
-    for (var i = 0; i < elements.length; i++) {
-        var htmlContent = elements[i].innerHTML;
-        var modifiedContent = htmlContent.replace(
-            /(?:<[^>]*>)|(\b[a-z])([a-z]+)?\b/gim,
-            function (match, p1, p2) {
-                if (p1) {
-                    return "<span class='first-letter'>" + p1 + "</span>" + (p2 || "");
-                } else {
-                    return match; 
-                }
-            }
-        );
-        elements[i].innerHTML = modifiedContent;
-    }
-};
+//     for (var i = 0; i < elements.length; i++) {
+//         var htmlContent = elements[i].innerHTML;
+//         var modifiedContent = htmlContent.replace(
+//             /(?:<[^>]*>)|(\b[a-z])([a-z]+)?\b/gim,
+//             function (match, p1, p2) {
+//                 if (p1) {
+//                     return "<span class='first-letter'>" + p1 + "</span>" + (p2 || "");
+//                 } else {
+//                     return match; 
+//                 }
+//             }
+//         );
+//         elements[i].innerHTML = modifiedContent;
+//     }
+// };
 // function addDarkmodeWidget() {
 //     new Darkmode().showWidget();
 //   }
