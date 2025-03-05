1400e684501b368d552dc5adfc220c15d75f1913
gimpoco1 <impoco126@gmail.com>
2025-03-05 11:26:45 +0100

    update experience calculation and update cv

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-AOS.init();
-function toggleMenu() {
-	const menu = document.querySelector(".menu-links");
-	const icon = document.querySelector(".hamburger-icon");
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
 
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const experienceElement = document.querySelector(
+    "#about .details-container p"
+  );
+
+  if (experienceElement) {
+    const startYear = 2022;
+    const currentYear = new Date().getFullYear();
+    const yearsOfExperience = currentYear - startYear;
+
+    experienceElement.innerHTML = `${yearsOfExperience}+ years <br />Full-Stack Development`;
+  }
+});
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
